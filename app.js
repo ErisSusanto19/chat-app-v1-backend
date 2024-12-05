3c3b92b76a972623ae4e608c314afe070e98ed81
Eris Susanto <erissusanto997@gmail.com>
2024-12-05 09:41:05 +0700

    create signup dan signin feature

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const router = require('./routes')
+const errorHandler = require('./middlewares/errorHandler')
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(router)
+app.use(errorHandler)
 
 connectDB().then(() => {
     app.listen(port, () => {
